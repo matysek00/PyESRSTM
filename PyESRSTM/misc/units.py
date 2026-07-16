c59a3d947453407973b5b5b1191006da60400a1f
matysek00 <matyas.nachtigall@gmail.com>
2026-07-16 11:59:05 +0200

    minor cleanup

--- PyESRSTM/misc/units.py
+++ PyESRSTM/misc/units.py
@@ -3,7 +3,7 @@
 e = 1 # a.u
 hbar = 1 # a.u
 Hartree = 27211.386245988 # meV
-BohrMagneton=5.7883818060E-5 # eV/T
+BohrMagneton = 5.7883818060E-5 # eV/T
 GHz = 4.135667696786E-3 # meV 
 pA = .6623618237510E10 # pA
 time_unit = 2.418884326505E-8 # ns
